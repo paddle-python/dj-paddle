3f054e3ed359b9fb1aa7d83148971a1c5a2c40bb
Matt Pye <pyematt@gmail.com>
2020-05-21 00:53:33 +0100

    Test invalid settings

---
+++ tests/test_settings.py
@@ -0,0 +1,56 @@
+import pytest
+from django.core.exceptions import ImproperlyConfigured
+
+
+def test_missing_djpaddle_public_key(settings):
+    settings.DJPADDLE_PUBLIC_KEY = None
+    from djpaddle import settings
+    from importlib import reload
+
+    with pytest.raises(ImproperlyConfigured):
+        reload(settings)
+
+
+def test_invalid_djpaddle_public_key(settings):
+    settings.DJPADDLE_PUBLIC_KEY = 123
+    from djpaddle import settings
+    from importlib import reload
+
+    with pytest.raises(ImproperlyConfigured):
+        reload(settings)
+
+
+def test_djpaddle_subscriber_model_invalid_name(settings):
+    settings.DJPADDLE_SUBSCRIBER_MODEL = "fakemodel"
+    from djpaddle import settings
+    from importlib import reload
+
+    reload(settings)
+    from djpaddle.settings import get_subscriber_model
+
+    with pytest.raises(ImproperlyConfigured):
+        get_subscriber_model()
+
+
+def test_djpaddle_subscriber_model_does_not_exist(settings):
+    settings.DJPADDLE_SUBSCRIBER_MODEL = "app_label.model_name"
+    from djpaddle import settings
+    from importlib import reload
+
+    reload(settings)
+    from djpaddle.settings import get_subscriber_model
+
+    with pytest.raises(ImproperlyConfigured):
+        get_subscriber_model()
+
+
+def test_missing_djpaddle_subscriber_model(settings):
+    settings.DJPADDLE_SUBSCRIBER_MODEL = "djpaddle.Price"
+    from djpaddle import settings
+    from importlib import reload
+
+    reload(settings)
+    from djpaddle.settings import get_subscriber_model
+
+    with pytest.raises(ImproperlyConfigured):
+        get_subscriber_model()
