45f083a279bf85c4d5ad773192ddaf002486c7b9
Matt Pye <pyematt@gmail.com>
2020-12-27 23:52:37 +0000

    Remove unneeded providing_args to silence RemovedInDjango40Warning

--- djpaddle/signals.py
+++ djpaddle/signals.py
@@ -1,28 +1,28 @@
 import django.dispatch
 
 # Subscription Alerts
-subscription_created = django.dispatch.Signal(providing_args=["payload"])
-subscription_updated = django.dispatch.Signal(providing_args=["payload"])
-subscription_cancelled = django.dispatch.Signal(providing_args=["payload"])
-subscription_payment_succeeded = django.dispatch.Signal(providing_args=["payload"])
-subscription_payment_failed = django.dispatch.Signal(providing_args=["payload"])
-subscription_payment_refunded = django.dispatch.Signal(providing_args=["payload"])
+subscription_created = django.dispatch.Signal()
+subscription_updated = django.dispatch.Signal()
+subscription_cancelled = django.dispatch.Signal()
+subscription_payment_succeeded = django.dispatch.Signal()
+subscription_payment_failed = django.dispatch.Signal()
+subscription_payment_refunded = django.dispatch.Signal()
 
 # One-off Purchases
-locker_processed = django.dispatch.Signal(providing_args=["payload"])
-payment_succeeded = django.dispatch.Signal(providing_args=["payload"])
-payment_refunded = django.dispatch.Signal(providing_args=["payload"])
+locker_processed = django.dispatch.Signal()
+payment_succeeded = django.dispatch.Signal()
+payment_refunded = django.dispatch.Signal()
 
 # Risk & Dispute Alerts
-payment_dispute_created = django.dispatch.Signal(providing_args=["payload"])
-payment_dispute_closed = django.dispatch.Signal(providing_args=["payload"])
-high_risk_transaction_created = django.dispatch.Signal(providing_args=["payload"])
-high_risk_transaction_updated = django.dispatch.Signal(providing_args=["payload"])
+payment_dispute_created = django.dispatch.Signal()
+payment_dispute_closed = django.dispatch.Signal()
+high_risk_transaction_created = django.dispatch.Signal()
+high_risk_transaction_updated = django.dispatch.Signal()
 
 # Payout Alerts
-transfer_created = django.dispatch.Signal(providing_args=["payload"])
-transfer_paid = django.dispatch.Signal(providing_args=["payload"])
+transfer_created = django.dispatch.Signal()
+transfer_paid = django.dispatch.Signal()
 
 # Audience Alerts
-new_audience_member = django.dispatch.Signal(providing_args=["payload"])
-update_audience_member = django.dispatch.Signal(providing_args=["payload"])
+new_audience_member = django.dispatch.Signal()
+update_audience_member = django.dispatch.Signal()
